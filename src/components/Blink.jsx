4743cae9c32819cf401ed3cf0cfdb5995e6d3df9
AndreiYar <yarovoi.a@do-it.co>
2020-12-24 13:57:57 +0200

    Recreated the tag.

--- src/components/Blink.jsx
+++ src/components/Blink.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 export const Blink = (props) => {
+    const [showlabel, setShowLabel] = useState(true);
+
+    const handleSHowLabel = useCallback(() => {
+        setShowLabel((prevShowLabel) => !prevShowLabel);
+    }, [setShowLabel])
+
+    useEffect(() => {
+        let timer = setInterval(() => {
+            handleSHowLabel();
+        }, 0.25);
+  
+        return () => clearTimeout(timer);
+    }, []);
+
     return(
-    <div className="react-blink">{props.children}</div>
+        <div className="react-blink">{showlabel ? props.children : null}</div>
     )
 }
